a950ca2ad7966697f5440417a7bfc28fab78948d
etrinidad <edward.trinidad132@gmail.com>
2022-02-17 11:38:14 -0500

    leetcode cont

--- algos/leetcode423.js
+++ algos/leetcode423.js
@@ -260,8 +260,8 @@ const z = "zero".split("");
 s = "fviefuro";
 // Output: "45"
 
-console.log(sortString("alpha"))
-console.log(sortString("zero"))
+// console.log(sortString("alpha"))
+// console.log(sortString("zero"))
 console.log(sequencer("alpha"))
 console.log(sequencer("aahlp"))
 
